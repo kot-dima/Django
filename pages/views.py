fdbe9b57fcf95d9a41d1949ae40f7c3b55693108
Дмитро Кот <Kot@rivne.itstep.org>
2020-03-12 20:56:29 +0200

    2

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,14 @@ from django.template.response import TemplateResponse
 
 
 def home(request):
-    return TemplateResponse(request, "home.html")
+    data = {
+        "h1": "Craft",
+        "h2": "Cover your page.",
+        "subtitle": "Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.",
+        
+
+    }
+    return TemplateResponse(request, "home.html", data)
 
 
 def about(request):
